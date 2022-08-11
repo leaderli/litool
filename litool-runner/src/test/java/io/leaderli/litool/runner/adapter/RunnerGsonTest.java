a47acc423b800aa36f39b6424c35bf5f7d4d3d37
leaderli <429243408@qq.com>
2022-08-12 01:13:47 +0800

    InstructTypeAdapter

--- litool-runner/src/test/java/io/leaderli/litool/runner/adapter/RunnerGsonTest.java
+++ litool-runner/src/test/java/io/leaderli/litool/runner/adapter/RunnerGsonTest.java
@@ -1,8 +1,8 @@
 package io.leaderli.litool.runner.adapter;
 
 import io.leaderli.litool.dom.parser.SaxEventInterceptor;
-import io.leaderli.litool.json.GsonUtil;
 import io.leaderli.litool.runner.xml.MainElement;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,16 +13,13 @@ class RunnerGsonTest {
 
     @Test
     void test() {
+
         SaxEventInterceptor<MainElement> dfs = new SaxEventInterceptor<>(MainElement.class);
         MainElement main = dfs.parse("main.xml");
-
         String json = RunnerGson.GSON.toJson(main);
-        System.out.println(json);
-
         main = RunnerGson.GSON.fromJson(json, MainElement.class);
 
-        GsonUtil.print(main);
-
+        Assertions.assertNotNull(main);
 
     }
 }
