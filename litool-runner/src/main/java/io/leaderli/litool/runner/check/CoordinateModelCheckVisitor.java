14a9142510992fe630db16e020fa25fc3e10390b
qin <wodewangyyouxiang@163.com>
2022-08-13 16:54:20 +0800

    coordinateModelCheckVisitor修改+++

--- litool-runner/src/main/java/io/leaderli/litool/runner/check/CoordinateModelCheckVisitor.java
+++ litool-runner/src/main/java/io/leaderli/litool/runner/check/CoordinateModelCheckVisitor.java
@@ -1,40 +1,50 @@
 package io.leaderli.litool.runner.check;
 
 import io.leaderli.litool.core.text.StringUtils;
+import io.leaderli.litool.dom.sax.SaxBean;
 import io.leaderli.litool.runner.TypeAlias;
+import io.leaderli.litool.runner.xml.router.task.CoordinateElement;
 
-public class CoordinateModelCheckVisitor extends ModelCheckVisitor {
+public class CoordinateModelCheckVisitor extends ElementCheckVisitor {
 
-    public CoordinateModelCheckVisitor(MainCheckVisitor mainCheckVisitor) {
-        super(mainCheckVisitor);
-    }
 
     @Override
-    public void request(String name, String id) {
-        mainElement().getRequest().entryList.lira()
-                .first(entry -> StringUtils.equals(name, entry.getKey()))
-                .ifPresent(entry -> addErrorMsgs(TypeAlias.getType(entry.getType()) == String.class,String.format("coordinate expression [%s] only support string %s", name, id)));
+    public void visit(CoordinateElement coordinate, SaxBean saxBean) {
+        Inner inner = new Inner();
+        ExpressionCheckVisitor visitor = new ExpressionCheckVisitor(inner);
+        
     }
 
-    @Override
-    public void response(String name, String id) {
-        mainElement().getResponse().entryList.lira()
-                .first(entry -> StringUtils.equals(name, entry.getKey()))
-                .ifPresent(entry -> addErrorMsgs(TypeAlias.getType(entry.getType()) == String.class,String.format("coordinate expression [%s] only support string %s", name, id)));
-    }
+    private static class Inner extends ModelCheckVisitor {
 
-    @Override
-    public void func(String name, String id) {
-        throw new UnsupportedOperationException();
-    }
 
-    @Override
-    public void temp(String name, String id) {
-        throw new UnsupportedOperationException();
-    }
+        @Override
+        public void request(String name, String id) {
+            mainElement.getRequest().entryList.lira()
+                    .first(entry -> StringUtils.equals(name, entry.getKey()))
+                    .ifPresent(entry -> addErrorMsgs(TypeAlias.getType(entry.getType()) == String.class, String.format("coordinate expression [%s] only support string %s", name, id)));
+        }
 
-    @Override
-    public void error(String name, String id) {
-        throw new UnsupportedOperationException();
+        @Override
+        public void response(String name, String id) {
+            mainElement.getResponse().entryList.lira()
+                    .first(entry -> StringUtils.equals(name, entry.getKey()))
+                    .ifPresent(entry -> addErrorMsgs(TypeAlias.getType(entry.getType()) == String.class, String.format("coordinate expression [%s] only support string %s", name, id)));
+        }
+
+        @Override
+        public void func(String name, String id) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void temp(String name, String id) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void error(String name, String id) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
