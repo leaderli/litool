f9d6a12f49211f9a71914f1d21b0c194d00d7361
leaderli <429243408@qq.com>
2022-09-23 01:05:09 +0800

    inputStream

--- litool-core/src/test/java/io/leaderli/litool/core/lang/ShellTest.java
+++ litool-core/src/test/java/io/leaderli/litool/core/lang/ShellTest.java
@@ -1,20 +1,12 @@
 package io.leaderli.litool.core.lang;
 
-import io.leaderli.litool.core.util.RandomUtil;
-import org.junit.jupiter.api.Test;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 /**
  * @author leaderli
  * @since 2022/9/22 9:24 AM
  */
 class ShellTest {
 
-    @Test
+    //    @Test
     void test() {
 
         System.out.println(System.getProperty("user.dir"));
@@ -26,21 +18,5 @@ class ShellTest {
 
     }
 
-    @Test
-    void test2() throws IOException {
-        System.out.println((int) '\n');
-        InputStream inputStream = new InputStream() {
-            @Override
-            public int read() {
-                if (RandomUtil.nextInt(50) == 10) {
-                    return 10;
-                }
-                return RandomUtil.nextInt('0', 'z');
-            }
-        };
 
-
-        System.out.println(new BufferedReader(new InputStreamReader(inputStream)).readLine());
-
-    }
 }
