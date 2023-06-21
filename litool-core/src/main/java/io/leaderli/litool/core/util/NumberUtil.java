d403a950a90c33430c3262614636b0bc8104f114
leaderli <429243408@qq.com>
2023-06-21 12:47:06 +0800

    添加 NumberUtil

---
+++ litool-core/src/main/java/io/leaderli/litool/core/util/NumberUtil.java
@@ -0,0 +1,26 @@
+package io.leaderli.litool.core.util;
+
+/**
+ * @author leaderli
+ * @since 2023/6/21 12:41 PM
+ */
+public class NumberUtil {
+
+    /**
+     * 值限制在给定范围内
+     *
+     * @param value 值
+     * @param min   最小值
+     * @param max   最大值
+     * @return 值限制在给定范围内
+     */
+    public static int constrain(int value, int min, int max) {
+        if (value < min) {
+            return min;
+        } else if (value > max) {
+            return max;
+        } else {
+            return value;
+        }
+    }
+}
---
+++ litool-core/src/test/java/io/leaderli/litool/core/util/NumberUtilTest.java
@@ -0,0 +1,22 @@
+package io.leaderli.litool.core.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author leaderli
+ * @since 2023/6/21 12:43 PM
+ */
+class NumberUtilTest {
+
+    @Test
+    void constrain() {
+        int result = NumberUtil.constrain(10, 0, 20);
+        assertEquals(10, result);
+        result = NumberUtil.constrain(-10, 0, 20);
+        assertEquals(0, result);
+        result = NumberUtil.constrain(30, 0, 20);
+        assertEquals(20, result);
+    }
+}
