ad0ed0eea19630b012c1c7ff0b263ffcf85e1e4d
leaderli <429243408@qq.com>
2023-07-02 23:35:00 +0800

    ast

--- litool-ast/src/main/java/io/leaderli/litool/ast/AstUtil.java
+++ litool-ast/src/main/java/io/leaderli/litool/ast/AstUtil.java
@@ -3,8 +3,12 @@ package io.leaderli.litool.ast;
 import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.SimpleName;
+import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.utils.SourceRoot;
 import io.leaderli.litool.core.exception.LiAssertUtil;
+import io.leaderli.litool.core.meta.Lira;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -12,12 +16,32 @@ import java.util.List;
 
 public class AstUtil {
 
-    public static List<CompilationUnit> getSources(String sourceDir) {
+    /**
+     * 解析工具目录的src目录的源码
+     *
+     * @return 源码目录的所有源码单元
+     */
+    public static List<CompilationUnit> getSources() {
         ParserConfiguration parserConfiguration = new ParserConfiguration();
         parserConfiguration.setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_8);
-        return getSources(parserConfiguration, sourceDir);
+        return getSources(parserConfiguration);
     }
 
+    /**
+     * 解析工具目录的src目录的源码
+     *
+     * @param parserConfiguration 解析规则
+     * @return 源码目录的所有源码单元
+     */
+    public static List<CompilationUnit> getSources(ParserConfiguration parserConfiguration) {
+        return getSources(parserConfiguration, System.getProperty("user.dir") + "/src");
+    }
+
+    /**
+     * @param parserConfiguration 解析规则
+     * @param sourceDir           源码目录,需要绝对目录
+     * @return 源码目录的所有源码单元
+     */
     public static List<CompilationUnit> getSources(ParserConfiguration parserConfiguration, String sourceDir) {
         SourceRoot sourceRoot = new SourceRoot(Paths.get(sourceDir), parserConfiguration);
         try {
@@ -31,21 +55,36 @@ public class AstUtil {
         return compilationUnits;
     }
 
-    public static List<CompilationUnit> getSources() {
+    /**
+     * @param sourceDir 源码目录,需要绝对目录
+     * @return 源码目录的所有源码单元
+     */
+    public static List<CompilationUnit> getSources(String sourceDir) {
         ParserConfiguration parserConfiguration = new ParserConfiguration();
         parserConfiguration.setLanguageLevel(ParserConfiguration.LanguageLevel.JAVA_8);
-        return getSources(parserConfiguration);
+        return getSources(parserConfiguration, sourceDir);
     }
 
-    public static List<CompilationUnit> getSources(ParserConfiguration parserConfiguration) {
-        return getSources(parserConfiguration, System.getProperty("user.dir") + "/src");
-    }
-
+    /**
+     * @param cu  源码
+     * @param cls 类
+     * @return 判断类是否通过import的方式导入了，如果导入了通配符，那么也视为导入。但是不包括全限定名
+     */
     public static boolean isImport(CompilationUnit cu, Class<?> cls) {
         String name = cls.getName();
         return !cu.findAll(ImportDeclaration.class, im -> {
             String importName = im.getNameAsString();
             return importName.equals(name) || im.isAsterisk() && name.startsWith(importName);
         }).isEmpty();
+    }
+
+    public static Lira<SimpleName> getClassDeclare(CompilationUnit cu, Class<?> cls) {
+
+        return Lira.of(cu.findAll(VariableDeclarator.class, v -> {
+            Type type = v.getType();
+            System.out.println(type.asString());
+//            return type == cls;
+            return true;
+        })).map(VariableDeclarator::getName);
     }
 }
---
+++ litool-ast/src/test/java/io/leaderli/litool/ast/TestBean.java
@@ -0,0 +1,16 @@
+package io.leaderli.litool.ast;
+
+
+import java.util.ArrayList;
+
+public class TestBean {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void m1() {
+        java.util.List<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+    }
+}
