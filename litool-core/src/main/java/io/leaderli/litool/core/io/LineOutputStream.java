ff2fe17438b097305fbe700e0be2b8f8f3e57ee0
leaderli <429243408@qq.com>
2023-10-02 17:46:05 +0800

    line outpustream

---
+++ litool-core/src/main/java/io/leaderli/litool/core/io/LineOutputStream.java
@@ -0,0 +1,52 @@
+package io.leaderli.litool.core.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.function.Consumer;
+
+public class LineOutputStream extends OutputStream {
+
+    private StringBuilder stringBuilder = new StringBuilder();
+
+    private final Consumer<String> readLine;
+    private final boolean skipLF;
+
+    public LineOutputStream() {
+        this.readLine = System.out::println;
+        this.skipLF = true;
+    }
+
+    public LineOutputStream(Consumer<String> readLine) {
+        this.readLine = readLine;
+        this.skipLF = true;
+    }
+
+    public LineOutputStream(Consumer<String> readLine, boolean skipLF) {
+        this.readLine = readLine;
+        this.skipLF = skipLF;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+
+        char ch = (char) b;
+
+        if (ch != '\n') {
+            if (ch == '\r' && skipLF) {
+                return;
+            }
+            stringBuilder.append(ch);
+        } else {
+            readLine.accept(stringBuilder.toString());
+            stringBuilder = new StringBuilder();
+        }
+    }
+
+    @Override
+    public void flush() {
+        if (stringBuilder.length() > 0) {
+            readLine.accept(stringBuilder.toString());
+            stringBuilder = new StringBuilder();
+        }
+    }
+}
