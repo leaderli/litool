d48ac9d46565d42a37e023d254d72669a867306d
leaderli <429243408@qq.com>
2024-03-02 03:41:35 +0800

    IOUtilsTest.java

--- litool-core/src/test/java/io/leaderli/litool/core/io/IOUtilsTest.java
+++ litool-core/src/test/java/io/leaderli/litool/core/io/IOUtilsTest.java
@@ -1,11 +1,14 @@
 package io.leaderli.litool.core.io;
 
+import io.leaderli.litool.core.meta.LiBox;
 import io.leaderli.litool.core.resource.ResourceUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 class IOUtilsTest {
 
@@ -24,5 +27,24 @@ class IOUtilsTest {
         IOUtils.copy(in, out1, out2);
         Assertions.assertEquals("123", out1.get());
         Assertions.assertEquals("123", out2.get());
+    }
+
+    @Test
+    void testEmptyPrintStream() {
+        LiBox<Integer> box = LiBox.none();
+        System.setErr(new PrintStream(new OutputStream() {
+            @Override
+            public void write(int b) {
+                box.value(b);
+            }
+        }));
+        System.err.print(1);
+        Assertions.assertEquals(49, box.value());
+        box.reset();
+
+        Assertions.assertNull(box.value());
+        System.setErr(IOUtils.emptyPrintStream());
+        System.err.print(1);
+        Assertions.assertNull(box.value());
     }
 }
