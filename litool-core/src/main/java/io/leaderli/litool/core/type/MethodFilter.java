5ede6e28c73babe3351601dc521cd90d5c038d67
leaderli <429243408@qq.com>
2024-03-03 23:22:35 +0800

    add MethodFilter

--- litool-core/src/main/java/io/leaderli/litool/core/type/MethodFilter.java
+++ litool-core/src/main/java/io/leaderli/litool/core/type/MethodFilter.java
@@ -4,6 +4,7 @@ import io.leaderli.litool.core.function.Chain;
 import io.leaderli.litool.core.function.Filter;
 import io.leaderli.litool.core.lang.FilterChain;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.Objects;
 
@@ -17,10 +18,6 @@ public class MethodFilter implements Filter<Method>, Chain<Filter<Method>> {
         return filter.apply(method);
     }
 
-
-    //    public static class Builder {
-//
-//
     public static MethodFilter isMethod() {
         return new MethodFilter().add(Objects::nonNull);
     }
@@ -37,26 +34,18 @@ public class MethodFilter implements Filter<Method>, Chain<Filter<Method>> {
         return new MethodFilter().add(filter);
     }
 
-    //    public MethodFilter name(String name) {
-//        return new MethodFilter().add(m -> m.getName().equals(name));
-//    }
-    //
-//        public void returnType(Class<?> returnType) {
-//            this.returnType = returnType;
-//        }
-//
-//        public void parameterType(Class<?>[] parameterType) {
-//            this.parameterType = parameterType;
-//        }
-//
-//        public void parameterlenth(Class<?>[] parameterlenth) {
-//            this.parameterlenth = parameterlenth;
-//        }
-//
-//        public void annotated(Class<? extends Annotation> annotated) {
-//            this.annotated = annotated;
-//        }
-//
+    public MethodFilter annotated(Class<? extends Annotation> annotated) {
+        return new MethodFilter().add(m -> m.isAnnotationPresent(annotated));
+    }
+
+    public MethodFilter returnType(Class<?> returnType) {
+        return new MethodFilter().add(m -> m.getReturnType() == returnType);
+    }
+
+    public MethodFilter length(int length) {
+        return new MethodFilter().add(m -> m.getParameterTypes().length == length);
+    }
+
     public void modifiers(int modifiers) {
     }
 
@@ -72,17 +61,52 @@ public class MethodFilter implements Filter<Method>, Chain<Filter<Method>> {
         return this;
     }
 
+    public Builder builder() {
+        return new Builder(this);
+    }
 
-//        public void isAbstract() {
-//            this.modifiers |= Modifier.ABSTRACT;
-//        }
-//
-//        public void isStatic() {
-//            this.modifiers |= Modifier.STATIC;
-//        }
+    public class Builder {
+        public final MethodFilter methodFilter;
 
+        public Builder(MethodFilter methodFilter) {
+            this.methodFilter = methodFilter;
+        }
 
-//    }
+        public Builder isMethod() {
+            methodFilter.add(Objects::nonNull);
+            return this;
+        }
 
+        public Builder name(String name) {
+            methodFilter.add(m -> m.getName().equals(name));
+            return this;
+        }
+
+        public Builder isPublic() {
+            methodFilter.add(ModifierUtil::isPublic);
+            return this;
+        }
+
+        public Builder of(Filter<Method> filter) {
+            methodFilter.add(filter);
+            return this;
+        }
+
+        public Builder annotated(Class<? extends Annotation> annotated) {
+            methodFilter.add(m -> m.isAnnotationPresent(annotated));
+            return this;
+        }
+
+        public Builder returnType(Class<?> returnType) {
+            methodFilter.add(m -> m.getReturnType() == returnType);
+            return this;
+        }
+
+        public Builder length(int length) {
+            methodFilter.add(m -> m.getParameterTypes().length == length);
+            return this;
+        }
+
+    }
 
 }
