5820ee0e7fe6a72739469b456d28cc7698b01ded
leaderli <429243408@qq.com>
2024-05-15 16:10:09 +0800

    增加异常默认值工具类

--- litool-core/src/test/java/io/leaderli/litool/core/exception/ExceptionUtilTest.java
+++ litool-core/src/test/java/io/leaderli/litool/core/exception/ExceptionUtilTest.java
@@ -90,6 +90,7 @@ class ExceptionUtilTest {
     @Test
     void supplier() {
         Assertions.assertEquals(1, ExceptionUtil.supplier(() -> 1 / (Integer.parseInt("1") - 1), 1));
+        Assertions.assertEquals(1, ExceptionUtil.supplier(() -> 1 / (Integer.parseInt("1") - 1), () -> 1));
         Assertions.assertEquals(1, (Integer) ExceptionUtil.function(i -> 1 / i, 1, i -> 1));
 
     }
